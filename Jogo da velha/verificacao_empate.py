edb034277647e56e3b5c0bb827d23011e361c888
renanassisg <renandeassisgomes@yahoo.com.br>
2025-10-13 10:34:46 -0300

    Atualização

--- Jogo da velha/verificacao_empate.py
+++
@@ -1,9 +0,0 @@
-tabuleiro = [["x", "o", "x"],
-             ["o", "o", "x"],
-             ["o", "x", "o"]]
-def verificacao_empate (matriz):
-    for i in range(len(matriz)):
-        for j in range(len(matriz[i])):
-            if matriz[i][j] == "_"
-                return True
-    retunr False
